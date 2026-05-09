07e191194efa7157c5d96baca10fc25d599e6ba1
ayubabenedict <ayubabenedict50@gmail.com>
2026-05-09 16:30:40 +0100

    files

---
+++ signin.js
@@ -0,0 +1,30 @@
+
+    let signInBtn = document.querySelector(".sign-in");
+
+    signInBtn.addEventListener("click", function () {
+
+        let email = document.querySelector("#email").value;
+        let password = document.querySelector("#password").value;
+        let savedUser = JSON.parse(localStorage.getItem("user"));
+
+        if (savedUser === null) {
+            alert("No account found!"
+                   +" Please sign up first ❌"
+            );
+        }
+
+        else if (
+            email === savedUser.email &&
+            password === savedUser.password
+        ) {
+
+            alert("Login successful ✅");
+
+            window.location.href = "dashboard.html";
+        }
+
+        else {
+            alert("Wrong email or password ❌");
+        }
+
+    });
---
+++ signup.js
@@ -0,0 +1,59 @@
+
+    let signUpBtn = document.querySelector(".sign-in");
+    signUpBtn.addEventListener("click", function () {
+        let fullName = document.querySelector("#fullName").value;
+        let email = document.querySelector("#email").value;
+        let password = document.querySelector("#psw").value;
+        let confirmPassword = document.querySelector("#confirmPsw").value;
+        if (
+            fullName === "" ||
+            email === "" ||
+            password === "" ||
+            confirmPassword === ""
+        ) {
+            alert("Please fill all fields");
+        }
+        // confirm password
+        else if (password !== confirmPassword) {
+            alert("Passwords do not match");
+        }
+
+        // save user details
+        else {
+            //    object to hold user details
+            let user = {
+                name: fullName,
+                email: email,
+                password: password
+            };
+            localStorage.setItem("user", JSON.stringify(user));
+
+            alert("Account created successfully ✅");
+            window.location.href = "sign.html";
+        }
+
+    });
+
+    // let click = document.querySelector("#nv2");
+    // let none = document.getElementById("nv1");
+    // click.addEventListener("click", function () {
+    //    none.style.display = "none";
+    // });
+
+
+    
+
+    // // getting the buttons
+    // let signUpBtn2 = document.querySelector("#nv2");
+    // let signInBtn1 = document.querySelector("#nv1");
+
+    // // when signup is clicked
+    // signUpBtn2.addEventListener("click", function () {
+
+    //     // hide signin button
+    //     signInBtn1.style.display = "none";
+
+    // });
+
+
+
